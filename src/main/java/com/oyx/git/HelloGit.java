c19e0677481129114929604689ccda946158be4b
whsxyoyx <2908895568@qq.coom>
2021-02-28 19:30:43 +0800

    更新

--- src/main/java/com/oyx/git/HelloGit.java
+++ src/main/java/com/oyx/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.oyx.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello  Git!!!");
+        System.out.println("edit_02");
     }
 }
